b4af60d813f5273ae84f7a7a8f97cad3f3c10930
Nils Adekambi <leretardatn@gmail.com>
2025-10-31 00:31:09 +0100

    Better code repartition

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*
+/**
  * `map.find()` which for some reason doesn't already exists.
  * @param {Map} map The map to look into.
  * @param searchedVal The value to look for.
@@ -10,156 +10,216 @@ function findMapValue(map, searchedVal) {
 	}
 	return null;
 }
-function execute_brainfuck() {
-	var code	 = document.body.querySelector('#input').value;
-	const output = document.body.querySelector('output');
-	output.innerText = '';
+
+/**
+ * Error utility function
+ * @param {String} msg 
+ */
+function error(msg) {
+	console.error(msg);
+	output.innerHTML = "<span class='error'>Error: "+ msg +"</span>";
+}
+
+/**
+ * @var {Map<int, int>} jojo
+ */
+var jojo;
+
+class InterpreterState {
+	/** @type {Map<int, int>} */
+	bracketCorrespondances;
+	/** @type {string} */
+	code;
+	/** @type {Uint8Array} */
+	memory;
+	/** @type {int} */
+	memPtr = 0;
+	/** @type {int} */
+	instructionPtr;
+	/** @type {bool} */
+	functionsEnabled = false;
 	
-	/** Error utility function */
-	function error(msg) {
-		console.error(msg);
-		output.innerHTML = "<span class='error'>Error: "+ msg +"</span>";
-	}
-	
-	let memory = new Uint8Array(30000);
-	let memPtr = 0;
-	
-	//read "preprocessor directives"
-	let funEnabled = false;
-	{
-		let fillWith = 0;
-		let memsize = 30000;
+	constructor() {
+		const input = document.body.querySelector('textarea');
+		if(input === null)
+			throw new Error("Element with id 'input' not defined.")
 		
-		while(code.startsWith("!")) {
-			if(code.startsWith("!init "))
-				fillWith = parseInt(/\d+/.exec(code)[0]);
-			else if(code.startsWith("!memsize "))
-				memsize = parseInt(/\d+/.exec(code)[0]);
-			else if(code.startsWith("!enablefunctions"))
-				funEnabled = true;
-			
-			memory = new Uint8Array(memsize);
-			code = code.slice(code.indexOf('\n')+1);
-		}
-		memory.fill(fillWith);
+		this.code	= input.value;
+		this.memory	= new Uint8Array(30000);
+		this.memPtr	= 0;
+		this.instructionPtr = 0;
+		this.bracketCorrespondances = new Map();
+		
+		this.readPreprocessors();
+		this.#initializeBracketMap()
 	}
 	
-	//initialize bracketMap[] (index opening bracket -> index closing bracket)
-	let bracketMap = new Map();
-	{
+	get instruction() {
+		return this.code[this.instructionPtr];
+	}
+	get nextInstruction() {
+		return this.code[this.instructionPtr + 1];
+	}
+	
+	get cellValue() {
+		return this.memory[this.memPtr];
+	}
+	/** @param {int} val */
+	set cellValue(val) {
+		this.memory[this.memPtr] = val;
+	}
+	
+	#initializeBracketMap() {
 		let encounteredOpeningBracketsPos = [];
-		for(let i in code) {
-			if(code[i] == '[')
+		for(let i in this.code) {
+			if(this.code[i] == '[')
 				encounteredOpeningBracketsPos.push(i);
-			if(code[i] == ']') {
+			if(this.code[i] == ']') {
 				if(encounteredOpeningBracketsPos.length == 0){
 					error("Unmatched ']' at index " + i.toString() + ".");
 					return
 				}
 				const openingIndex = encounteredOpeningBracketsPos.pop();
-				bracketMap.set(parseInt(openingIndex), parseInt(i));
+				this.bracketCorrespondances.set(parseInt(openingIndex), parseInt(i));
 			}
 		}
-		if(encounteredOpeningBracketsPos.length != 0){
+		if(encounteredOpeningBracketsPos.length != 0) {
 			error("Unmatched '[' at indexes " + encounteredOpeningBracketsPos.toString() + ".")
 			return;
 		}
 	}
 	
+	readPreprocessors() {
+		let fillWith = 0;
+		let memsize = 30000;
+		
+		while(this.code.startsWith("!")) {
+			if(this.code.startsWith("!init "))
+				fillWith = parseInt(/\d+/.exec(this.code)[0]);
+			
+			else if(this.code.startsWith("!memsize "))
+				memsize = parseInt(/\d+/.exec(this.code)[0]);
+			
+			else if(this.code.startsWith("!enablefunctions"))
+				this.functionsEnabled = true;
+			
+			this.memory = new Uint8Array(memsize);
+			this.code = this.code.slice(this.code.indexOf('\n')+1);
+		}
+		this.memory.fill(fillWith);
+	}
+}
+
+/**
+ * If they are enabled, interpret the funcrions.
+ * @param {InterpreterState} state
+ * @returns {bool} if the function ended withput error any.
+ */
+function interpretFunctions(state) {
+	if(!state.functionsEnabled)
+		return;
+	
+	for(const fun of ["moveto", "putchar"]) {
+		if(state.code.length <= fun.length + state.instructionPtr + 2)
+			continue;
+		if(state.code.slice(state.instructionPtr + 1, fun.length + state.instructionPtr + 1) != fun || state.code[fun.length + state.instructionPtr + 1] != '(')
+			continue;
+		
+		state.instructionPtr += fun.length + 2;
+		switch(fun) {
+			case "moveto":
+				if(!/\d/.test(state.instruction)) {
+					error("moveto() expects 1 integer argument.");
+					return false;
+				}
+				
+				const addressStr = /\d+/.exec(state.code.slice(state.instructionPtr))[0];
+				state.instructionPtr += addressStr.length;
+				if(state.instruction != ')'){
+					error("moveto() expects only 1 integer argument.");
+					return false;
+				}
+				
+				state.memPtr = parseInt(addressStr);
+				break;
+			case "putchar":
+				if(state.nextInstruction != ')') {
+					error("put_char() expects 1 charcter.");
+					return false;
+				}
+				
+				state.cellValue = state.instruction.charCodeAt(0);
+				state.instructionPtr++;
+				break;
+			default:
+				error(`Unknown function "${fun}()".`);
+				return false;
+		}
+		//instructionPtr should point on the closing parenthesis
+		//if adding something in the case, declare a variable to see if the exit was because of an error.
+	}
+	return true;
+}
+
+
+/**
+ * Executes the brainfuck with 
+ * @param {InterpreterState} state 
+ */
+function execute_brainfuck(state = new InterpreterState()) {
+	const output = document.body.querySelector('output');
+	output.innerText = '';
+	
 	//main loop
-	for(let instructionPtr = 0; instructionPtr < code.length; instructionPtr++) {
-		switch(code[instructionPtr]) {
+	for(; state.instructionPtr < state.code.length; state.instructionPtr++) {
+		switch(state.instruction) {
 			case '>':
-				if(++memPtr >= memory.length) {
-					error(`Memory pointer too large (over ${memory.length}).`)
+				if(++state.memPtr >= state.memory.length) {
+					error(`Memory pointer too large (over ${state.memory.length}).`)
 					return;
 				}
 				break;
 			case '<':
-				if(--memPtr < 0) {
+				if(--state.memPtr < 0) {
 					error("Memory pointer negative.")
 					return;
 				}
 				break;
 			case '+':
-				memory[memPtr]++;
+				state.cellValue++;
 				break;
 			case '-':
-				memory[memPtr]--;
+				state.cellValue--;
 				break;
 			case ',':
-				memory[memPtr] = prompt("Enter a character.")[0].charCodeAt(0); //unicode? Who uses it?
+				state.cellValue = prompt("Enter a character.")[0].charCodeAt(0); //unicode? Who uses it?
 				break;
 			case '.':
-				output.innerText += String.fromCharCode(memory[memPtr]);
+				output.innerText += String.fromCharCode(state.cellValue);
 				break;
 			case '[':
-				if(memory[memPtr])
+				if(state.cellValue)
 					break;
 				
-				instructionPtr = bracketMap.get(instructionPtr);
+				state.instructionPtr = state.bracketCorrespondances.get(state.instructionPtr);
 				break;
 			case ']':
-				if(!memory[memPtr])
+				if(!state.cellValue)
 					break;
 				
-				const newPtr = findMapValue(bracketMap, instructionPtr);
+				const newPtr = findMapValue(state.bracketCorrespondances, state.instructionPtr);
 				if(newPtr === null) {
-					error("Matching bracket not found for bracket at index " + instructionPtr + ".");
-					console.debug(bracketMap);
+					error(`Matching bracket not found for bracket at index "${state.instructionPtr}.`);
 					return;
 				}
-				instructionPtr = newPtr;
+				state.instructionPtr = newPtr;
 				break;
 			case '$':
-				if(!funEnabled)
-					break;
-				for(const fun of ["moveto", "putchar"]) {
-					if(code.length <= fun.length + instructionPtr + 2)
-						continue;
-					if(code.slice(instructionPtr + 1, fun.length + instructionPtr + 1) != fun || code[fun.length + instructionPtr + 1] != '(')
-						continue;
-					
-					instructionPtr += fun.length + 2;
-					switch(fun){
-						case "moveto":
-							if(!/\d/.test(code[instructionPtr])) {
-								error("moveto() expects 1 integer argument.");
-								return;
-							}
-							
-							const addressStr = /\d+/.exec(code.slice(instructionPtr))[0];
-							instructionPtr += addressStr.length;
-							if(code[instructionPtr] != ')'){
-								error("moveto() expects only 1 integer argument.");
-								return;
-							}
-							
-								
-							memPtr = parseInt(addressStr);
-							break;
-						case "putchar":
-							if(code[instructionPtr+1] != ')') {
-								error("put_char() expects 1 charcter.");
-								return;
-							}
-							
-							console.log(code[instructionPtr]);
-							memory[memPtr] = code[instructionPtr].charCodeAt(0);
-							instructionPtr++;
-							break;
-						default:
-							error(`Unknown function "${fun}()".`);
-							return;
-					}
-					//instructionPtr should point on the closing parenthesis
-					//if adding something in the case, declare a variable to see if the exit was because of an error.
-					break;
-				}
-					
+				if(!interpretFunctions(state))
+					return;
 			default:
 				continue;
 		}
 	}
-	console.info(memory);
+	console.info(state.memory);
 }
